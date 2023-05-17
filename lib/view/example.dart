1f7fe3e993c48399b5be7690161718ffea04bca9
Franck Obité <obitefrank@gmail.com>
2023-05-17 09:12:42 +0000

    second commit

--- lib/view/example.dart
+++ lib/view/example.dart
@@ -10,6 +10,28 @@ class Example extends StatefulWidget {
 }
 
 class _ExampleState extends State<Example> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = VideoPlayerController.asset(
+        "assets/images/IMG_20230516_145753_202.mp4");
+    _controller.initialize().then((value) {
+      setState(() {});
+    });
+    _controller.play();
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +39,7 @@ class _ExampleState extends State<Example> {
       fit: StackFit.expand,
       children: [
         Expanded(
-          child: Text(""), /*  */
+          child: VideoPlayer(_controller), /*  */
         ),
         const Text("Bienvenue à SOABanque mobile")
       ],
